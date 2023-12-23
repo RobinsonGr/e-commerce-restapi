644772a0241af069a7af2966b59e6fd798efa269
Robinson Gr <77526151+RobinsonGr@users.noreply.github.com>
2023-12-22 23:58:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const session = require('express-session');
+
+const authSetup = require('./config/passport');
+
+authSetup(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  session({
+    secret: '123',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
