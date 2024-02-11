a8e7f32ca4b2b33006ae2bda932e59b756416bd9
Robinson Gr <77526151+RobinsonGr@users.noreply.github.com>
2024-02-10 23:56:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,34 @@ app.use('/user', require('./routes/users'));
 app.listen(3000, () => {
   console.log('listening at port 3000')
 })
+
+//
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const session = require('express-session');
+
+const authSetup = require('./config/passport');
+
+authSetup(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  session({
+    secret: '123',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/products', require('./routes/index'));
+app.use('/categories', require('./routes/categories'));
+app.use('/user', require('./routes/users'));
+        
+app.listen(3000, () => {
+  console.log('listening at port 3000')
+})
