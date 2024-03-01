bae12b9b8f5db13a4a79c3ee076f18fed1599353
RobinsonGr <robinsonandresgr@gmail.com>
2024-02-29 23:55:47 -0500

    s

--- controller/categories.controller.js
+++ controller/categories.controller.js
@@ -59,3 +59,66 @@ module.exports = {
     addCategories,
     deleteCategories
 }
+
+///
+const pool = require('../config/database.js')
+
+// Retrieves all categories from the db
+async function getCategories(req, res) {
+  try {
+    const response = await pool.query(
+        `SELECT *
+        FROM categories `
+    );
+
+    res.json(response.rows)
+  } catch(err) { 
+    console.error('Failed to delete category:', err.message);
+    res.status(500).json({ error: 'Failed to delete category'});
+  }
+};
+
+//adds a new category to the db
+async function addCategories (req, res) {
+    const {id, name} = req.body;
+
+    try {
+        const response = await pool.query(
+            `INSERT INTO categories
+            VALUES($1, $2)
+            RETURNING *
+            `, 
+            [id, name]
+        );
+
+        res.json(response.rows);
+    } catch(err) {
+        console.error('Failed to delete category:', err.message);
+        res.status(500).json({ error: 'Failed to delete category' });
+    }
+};
+
+//deletes a category from the db with the id as parameter
+async function deleteCategories (req, res) {
+    const {id} = req.params;
+
+    try {
+        const response = await pool.query(`
+        DELETE FROM categories
+        WHERE category_id = $1
+        RETURNING *
+        `, 
+        [id]);
+
+        return res.json(response.rows);
+    } catch (err) {
+        console.error('Failed to delete category:', err.message)
+        res.status(500).json({ error: 'Failed to delete category' });
+    };
+};
+
+module.exports = {
+    getCategories,
+    addCategories,
+    deleteCategories
+}
