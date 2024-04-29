44383ae3b16aa025cc810793c7f4f8d7beba822c
RobinsonGr <robinsonandresgr@gmail.com>
2024-04-28 23:55:12 -0500

    testing endpoints

--- db/db.sql
+++
@@ -1,7 +0,0 @@
- SELECT products.*, categories.name
-    FROM products
-    LEFT JOIN categories_products 
-    ON products.id = categories_products.product_id
-    LEFT JOIN categories
-    ON categories_products.category_id = categories.id
-    WHERE categories.url = 'organic-food';
